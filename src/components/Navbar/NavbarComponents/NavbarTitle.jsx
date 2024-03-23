9ea39fe4434124dd3b04d5d6b209e551bfce350b
Maciej Kubicki <dev.kubicki@gmail.com>
2024-03-23 13:51:11 +0100

    working on navbar

--- src/components/Navbar/NavbarComponents/NavbarTitle.jsx
+++ src/components/Navbar/NavbarComponents/NavbarTitle.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 export default function NavbarTitle({ text, highlightText }) {
+  // const [widthSite, setWidthSite] = useState(window.innerWidth);
+  const [widthSite, setWidthSite] = useState(window.innerHeight);
+
+  function handleResize() {
+    // setWidthSite(window.innerWidth);
+    setWidthSite(window.innerHeight);
+    console.log(widthSite);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div style={{ position: "relative", top: 40, left: 20 }}>
+    <div style={{ position: "relative", top: 40, left: 160 }}>
       <h1
         style={{
           color: "white",
