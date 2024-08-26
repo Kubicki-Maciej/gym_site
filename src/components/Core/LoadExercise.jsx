8d049313508adecd8234d1c593f31ee1f16ae5c6
unknown <dev.kubicki@gmail.com>
2024-08-26 11:46:02 +0200

    start working on branch trening

---
+++ src/components/Core/LoadExercise.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LoadExercise({loading, error, setExercise}) {
+  return (
+    <div>LoadExercise</div>
+  )
+}
--- src/components/Exercise/CreateNewTrening.jsx
+++ src/components/Exercise/CreateNewTrening.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import NavigateButton from '../Buttons/MainButton'
 
+import Searcher from '../Core/Searcher';
+
 export default function CreateNewTrening() {
+  const [exerciseObject , setExerciseObject] = useState({});
+  const [exerciseName, setExerciseName] = useState('');
+  const [exerciseDescription, setExerciseDescription] = useState('');
+  const [muscleGroups, setMuscleGroups] = useState([]);
+  const [muscleGroupOptions, setMuscleGroupOptions] = useState([]);
+
+
+
+  function getExerciseFromSercher(exercise){
+    if(exercise){
+      console.log('exercise.muscle_group');
+      console.log(exercise.muscle_group);
+      setExerciseObject(exercise)
+      setExerciseName(exercise.name)
+      setExerciseDescription(exercise.description);
+      setMuscleGroups(
+        muscleGroupOptions.filter((group) =>
+        exercise.muscle_group.includes(group.id)
+      ));
+    }else{
+      setExerciseName('');
+      setExerciseDescription('');
+      setMuscleGroups([]);
+    }
+  }
+  
+
   return (
-    <div>
-        CreateNewTrening
-        <br/>
-        <NavigateButton navigateDir={'createexercise/'} name={"Create New Exercise"}/>
-        <br/>
-        <button>Add exercise to trening</button>
+    <div style={{
+      display:"flex",
+      flexDirection:"column",
+      maxWidth:"400px",
+      margin:"auto"
+  }}>
+        <div>
+          {/* WYBIERZ TRENING LUB STWORZ NOWY  */}
+        </div>
+        <div>
+        {/* NAZWA NOWEGO TRENINGU */}
+        </div>
+        
+        <div>
+          {/* #LISTA CWICZEN */}
+        </div>
+        
+        <div>
+          {/* ELEMENT ODPOWIADA ZA SZUKANIE CWICZEN */}
+          <Searcher dataOutput={getExerciseFromSercher}/>
+        </div>
+        <button>PRZYCISK DO DODANIA CWICZENIA DO TRENINGU</button>
+        <div>
+          {/* NAVIGACYJNY PRZYCISK BY ZROBIC NOWE CWICZENIE  */}
+          <NavigateButton navigateDir={'createexercise/'} name={"Create New Exercise"}/>
+        </div>
     </div>
+
   )
 }
