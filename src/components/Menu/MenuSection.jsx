24f8fb1052df184156916940d26d540654777888
unknown <dev.kubicki@gmail.com>
2026-02-12 17:34:33 +0100

    cleaned old imports

--- src/components/Menu/MenuSection.jsx
+++ src/components/Menu/MenuSection.jsx
@@ -1,19 +1,3 @@
-import { Routes, Route } from "react-router-dom";
-
-import UserAddTraining from "../../Screens/Training/Forms/UserAddTraining";
-import CreateWorkout from "../Trening/CreateWorkout";
-import UserProfile from "../../features/users/components/UserProfile";
-import { StudentsPage } from "../../features/students/pages/StudentPage";
-
-import EditTrening from "../Trening/EditTrening";
-import CreateExercise from "../Exercise/CreateExercise";
-import CreateNewTraining from "../Trening/CreateTrening";
-
-import StudentProfileScreen from "../../Screens/StudentProfileScreen";
-import StudentNextTraining from "../Student/StudentNextTraining";
-import StudentTrainingsScreen from "../Student/StudentTrainingsScreen";
-import StudentCreateTraining from "../Student/StudentCreateTraining";
-
 import MenuRoutes from "../../router/MenuRoutes";
 
 export default function MenuSection({ sideBarName }) {
