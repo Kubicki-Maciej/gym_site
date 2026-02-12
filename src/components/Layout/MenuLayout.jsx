22b8cb7bed9a5956aa0aef8c6dacff7b4a1a0ecd
unknown <dev.kubicki@gmail.com>
2026-02-12 18:12:47 +0100

    created MenuLayout it replace MenuScreen

---
+++ src/components/Layout/MenuLayout.jsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import MenuSidebar from "../../components/Menu/MenuSidebar";
+import MenuMobileBottom from "../../components/Layout/Navbar/MenuMobileButton";
+import MenuRoutes from "../../router/MenuRoutes";
+
+export default function MenuLayout({ sideBarName }) {
+  return (
+    <Box sx={{ display: "flex", minHeight: "100vh" }}>
+      <MenuSidebar sideBarName={sideBarName} />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          pb: { xs: "56px", md: 3 },
+          width: { md: `calc(100% - 280px)` },
+        }}
+      >
+        <MenuRoutes sideBarName={sideBarName} />
+      </Box>
+      <MenuMobileBottom sideBarName={sideBarName} />
+    </Box>
+  );
+}
