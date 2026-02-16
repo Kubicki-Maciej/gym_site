30c5dde127f6dbf115e43693f004a1641843107a
unknown <dev.kubicki@gmail.com>
2026-02-17 00:28:20 +0100

    changed geting selectedUser from context into from LStorage

--- src/features/statistics/hooks/useUserExerciseInDateRangeStatistic.jsx
+++ src/features/statistics/hooks/useUserExerciseInDateRangeStatistic.jsx
@@ -8,18 +8,6 @@ export default function useUserExerciseInDateRangeStatistic({
   endDate,
   enabled = true,
 }) {
-  const shouldFetch =
-    enabled && !!exerciseId && !!userId && !!startDate && !!endDate;
-  console.log("userId w hooku", userId);
-  console.log("=== HOOK DEBUG ===");
-  console.log("exerciseId:", exerciseId);
-  console.log("userId:", userId);
-  console.log("startDate:", startDate);
-  console.log("endDate:", endDate);
-  console.log("enabled (przekazane):", enabled);
-  console.log("shouldFetch (finalne):", shouldFetch);
-  console.log("==================");
-
   return useQuery({
     queryKey: [
       "user-statistics-exercise-in-date-range",
