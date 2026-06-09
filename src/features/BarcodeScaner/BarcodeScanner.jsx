8e28824b234c9845e0a09ce68c0d82a317525168
unknown <M kubicki Alexandra PC>
2026-06-09 14:53:22 +0200

    cammera fix

--- src/features/BarcodeScaner/BarcodeScanner.jsx
+++ src/features/BarcodeScaner/BarcodeScanner.jsx
@@ -4,11 +4,15 @@ import { Box, Typography } from "@mui/material";
 export default function BarcodeScanner({ onScan }) {
   console.log("isSecureContext:", window.isSecureContext);
   console.log("mediaDevices:", navigator.mediaDevices);
+  const [result, setResult] = useState("");
   const { ref } = useZxing({
     paused: false,
+     onDecodeResult(result) {
+      setResult(result.rawValue);
+    },
     constraints: {
       video: {
-        facingMode: { ideal: "environment" }, // tylna kamera
+        facingMode: { ideal: "environment" }, 
       },
       audio: false,
     },
@@ -32,6 +36,7 @@ export default function BarcodeScanner({ onScan }) {
         muted
         playsInline
       />
+      <p>{result}</p>
       <Typography variant="caption">Skieruj kamerę na kod kreskowy</Typography>
     </Box>
   );
