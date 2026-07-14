3ccd53fa625d5b4f1965117341efa70e033cb225
unknown <dev.kubicki@gmail.com>
2026-07-14 12:23:28 +0200

    working on leayout style ean

--- src/config.jsx
+++ src/config.jsx
@@ -1,3 +1,3 @@
-export const API_URL = "https://devkubicki.pythonanywhere.com/";
-// export const API_URL = "http://127.0.0.1:8000/";
+// export const API_URL = "https://devkubicki.pythonanywhere.com/";
+export const API_URL = "http://127.0.0.1:8000/";
 // export const API_URL = "http://192.168.1.12:8000/";
